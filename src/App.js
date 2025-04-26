21a53f7f880ac93c534b8c2a6c1e28816989b64d
gitLunga <lungamngomezulu10@gmail.com>
2025-04-26 11:05:53 +0200

    initial changes

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Register from './screens/Register';
+import Login from './screens/Login';
+import PlayerForm from './component/PlayerForm';
+
+import AdminDashboard from './component/AdminDashboard';
+
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Register />} />
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/playerform' element={<PlayerForm />} />
+
+        <Route exact path='/AdminDashboard' element={<AdminDashboard />} />
+
+
+
+      </Routes>
+    </Router>
+  );
+}
+export default App;
